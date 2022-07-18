7c652d910f6c3475936cd3791a3a1fabed282080
LukaPatarcic <luka.patarcic@auxality.com>
2022-07-18 22:19:11 +0200

    added some html elements

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    images: {
+        domains: ['i.imgflip.com'],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
 import type { NextPage } from 'next';
 
+import Image from 'next/image';
+
 const Home: NextPage = () => {
-    return <div>Meme Generator</div>;
+    return (
+        <div>
+            <Image
+                src="https://i.imgflip.com/1bij.jpg"
+                alt="meme"
+                width={568}
+                height={335}
+            />
+            {Array(3)
+                .fill(0)
+                .map((_, index) => (
+                    <input key={index} className="border border-black" />
+                ))}
+            Meme Generator
+        </div>
+    );
 };
 
 export default Home;
